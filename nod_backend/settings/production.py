23a91b4eaf4a5030d901d021e74f148a18b68233
eliataylor <eli@taylormadetraffic.com>
2024-06-20 16:54:02 -0700

    pre deploy configs

--- nod_backend/settings/production.py
+++ nod_backend/settings/production.py
@@ -5,17 +5,14 @@ DEBUG = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ["SECRET_KEY"]
-SECURE_SSL_REDIRECT=True
-SESSION_COOKIE_SECURE=True
-# Ensure HTTPS is used
 SECURE_SSL_REDIRECT = True
-CSRF_COOKIE_SECURE=True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Set HSTS headers
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
 SECURE_HSTS_PRELOAD = True  # Allow the site to be included in browsers' HSTS preload list
-
 
 try:
     from .local import *
