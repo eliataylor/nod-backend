6b4c46613573f09e5de51eac73bc42551bdefdad
eliataylor <eli@taylormadetraffic.com>
2024-06-20 23:24:15 -0700

    --check passed

--- nod_backend/settings/dev.py
+++ nod_backend/settings/dev.py
@@ -27,6 +27,21 @@ ALLOWED_HOSTS = [
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 
+# Default storage settings, with the staticfiles storage updated.
+# See https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-STORAGES
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    # ManifestStaticFilesStorage is recommended in production, to prevent
+    # outdated JavaScript / CSS assets being served from cache
+    # (e.g. after a Wagtail upgrade).
+    # See https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
+
 try:
     from .local import *
 except ImportError:
