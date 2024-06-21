4e21fe4e2c1dabf0400337607a87f4e961707180
eliataylor <eli@taylormadetraffic.com>
2024-06-20 17:07:13 -0700

    pre deploy configs

--- nod_backend/settings/production.py
+++ nod_backend/settings/production.py
@@ -1,4 +1,7 @@
 from .base import *
+import os
+from google.oauth2 import service_account
+
 
 DEBUG = False
 
@@ -13,6 +16,18 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
 SECURE_HSTS_PRELOAD = True  # Allow the site to be included in browsers' HSTS preload list
+
+
+# Google Cloud Storage settings
+GS_BUCKET_NAME = 'nod_media'
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, 'path/to/your-service-account-file.json')
+)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_DEFAULT_ACL = 'publicRead'
+
+MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
 
 try:
     from .local import *
