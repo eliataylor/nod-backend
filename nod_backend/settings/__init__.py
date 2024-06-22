7dd944c6d6908477dbfda72544bda627297a38e8
Abdul Aziz <aziz@blogdoang.com>
2024-06-22 15:13:39 +0700

    Temporary disable dev

--- nod_backend/settings/__init__.py
+++ nod_backend/settings/__init__.py
@@ -3,12 +3,14 @@ import os
 # Determine environment
 DJANGO_ENV = os.getenv('DJANGO_ENV', 'dev')
 
+from .production import *
+
 # Load appropriate settings
-if DJANGO_ENV == 'dev':
-    print(f"DJANGO_ENV: {os.getenv('DJANGO_ENV')} is SET")
-    from .dev import *
-elif DJANGO_ENV == 'production':
-    print(f"DJANGO_ENV: {os.getenv('DJANGO_ENV')} is SET")
-    from .production import *
-else:
-    raise ImportError("Invalid DJANGO_ENV setting")
+# if DJANGO_ENV == 'dev':
+#     print(f"DJANGO_ENV: {os.getenv('DJANGO_ENV')} is SET")
+#     from .dev import *
+# elif DJANGO_ENV == 'production':
+#     print(f"DJANGO_ENV: {os.getenv('DJANGO_ENV')} is SET")
+#     from .production import *
+# else:
+#     raise ImportError("Invalid DJANGO_ENV setting")
