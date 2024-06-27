4ed01c1fad060bb07a01c2156ccf96cb92fe4f6f
ricardo <ricardotaggle1006@gmail.com>
2024-06-27 22:31:44 +0800

    Add Swagger documentation

--- nod_backend/urls.py
+++ nod_backend/urls.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
-from django.views.generic.base import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 ####OBJECT-ACTIONS-URL-IMPORTS-ENDS####
 
@@ -10,11 +12,21 @@ from django.views.generic.base import TemplateView
 
 ####OBJECT-ACTIONS-URLS-STARTS####
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Nod Backend APIs",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("nod_app.urls")),
     path('auth/', include("users.urls", namespace="users")),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
